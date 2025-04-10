62ab74fdc2aa04fea9a478e900cd2e0005b75bf4
saidamst2003 <saida2003mst@gmail.com>
2025-04-11 00:12:29 +0100

    j'ai terminer la creation de component de experiencee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import{NavbarComponent} from './navbar/navbar.component';
-import{MaisonComponent} from'./maison/maison.component'
+import {NavbarComponent} from './navbar/navbar.component';
+import {MaisonComponent} from'./maison/maison.component';
+import {AproposComponent}from'./apropos/apropos.component';
+import {ExperienceeComponent}from'./experiencee/experiencee.component'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,MaisonComponent],
+  imports: [RouterOutlet,NavbarComponent,MaisonComponent,AproposComponent,ExperienceeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
