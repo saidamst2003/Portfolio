2ac2bb01e56f770b25643711099c949362833138
saidamst2003 <saida2003mst@gmail.com>
2025-04-11 15:21:34 +0100

    j'ai  terminer conponent parcours

---
+++ src/app/competences/competences.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competences',
+  imports: [],
+  templateUrl: './competences.component.html',
+  styleUrl: './competences.component.css'
+})
+export class CompetencesComponent {
+
+}
